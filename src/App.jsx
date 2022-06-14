f204e29f1819ec300eba1e32ff5a8f1f5731c38a
Lacie Covington <lacie.covington@gmail.com>
2022-06-14 14:04:30 -0400

    edited app chip header instructions and nutritional label files

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import * as React from "react"
 // IMPORT ANY NEEDED COMPONENTS HERE
+import Header from "./components/Header/Header"
+import Instructions from "./components/Instructions/Instructions"
+import Chip from "./components/Chip/Chip"
+import NutritionalLabel from "./components/NutritionalLabel/NutritionalLabel"
+import { useState } from "react"
 import { createDataSet } from "./data/dataset"
+
 import "./App.css"
 
 // don't move this!
@@ -21,37 +27,56 @@ export const appInfo = {
 const { data, categories, restaurants } = createDataSet()
 
 export function App() {
+
+  const [selectedCategory, setCategory] = React.useState(0)
+  const [selectedRestaurant, setRestaurant] = React.useState(0)
+  const [selectedItem, setItem] = React.useState(0)
+
+  const currentMenuItems = data.filter((menuItem) => {
+    return menuItem.food_category == selectedCategory && menuItem.restaurant == selectedRestaurant
+  })
+
   return (
     <main className="App">
       {/* CATEGORIES COLUMN */}
       <div className="CategoriesColumn col">
         <div className="categories options">
           <h2 className="title">Categories</h2>
-          {/* YOUR CODE HERE */}
+          {categories.map((category) =>
+          <Chip key={category} label={category} onClick={() => setCategory(category)} isActive={selectedCategory == category} />
+        )}
         </div>
       </div>
 
       {/* MAIN COLUMN */}
       <div className="container">
         {/* HEADER GOES HERE */}
-
+        <Header title={appInfo.title} tagline={appInfo.tagline} description={appInfo.description}/>
         {/* RESTAURANTS ROW */}
         <div className="RestaurantsRow">
           <h2 className="title">Restaurants</h2>
-          <div className="restaurants options">{/* YOUR CODE HERE */}</div>
+          <div className="restaurants options">
+          {restaurants.map((restaurant) => 
+          <Chip key={restaurant} label={restaurant} onClick={() => setRestaurant(restaurant)} isActive={selectedRestaurant == restaurant} />
+        )}
+          </div>
         </div>
 
         {/* INSTRUCTIONS GO HERE */}
-
+        <Instructions instructions={appInfo.instructions.start}/>
         {/* MENU DISPLAY */}
         <div className="MenuDisplay display">
           <div className="MenuItemButtons menu-items">
             <h2 className="title">Menu Items</h2>
-            {/* YOUR CODE HERE */}
+            {currentMenuItems.map((menuItem) => (
+              <Chip key={menuItem.item_name} label={menuItem.item_name} onClick={() => setItem(menuItem)} isActive={selectedItem.item_name == menuItem.item_name} />
+            ))}
           </div>
 
           {/* NUTRITION FACTS */}
-          <div className="NutritionFacts nutrition-facts">{/* YOUR CODE HERE */}</div>
+          <div className="NutritionFacts nutrition-facts">
+            <NutritionalLabel item={selectedItem} />
+          </div>
         </div>
 
         <div className="data-sources">
--- src/components/Chip/Chip.jsx
+++ src/components/Chip/Chip.jsx
@@ -1,13 +1,22 @@
 import * as React from "react"
+import { useState } from "react"
 import "./Chip.css"
 
-export function Chip({ label = "", isActive = false }) {
+export function Chip({ label = "", isActive = false, onClick = () => {}}) {
+
+  let buttonClassName;
+
+  if (isActive) {
+    buttonClassName = "chip active"
+  } else {
+    buttonClassName = "chip"
+  }
+
   return (
-    <button className="chip">
-      <p className="label"></p>
+    <button className={buttonClassName} onClick={onClick}>
+      <p className="label">{label}</p>
       <span className="close" role="button">{`X`}</span>
     </button>
   )
 }
-
 export default Chip
